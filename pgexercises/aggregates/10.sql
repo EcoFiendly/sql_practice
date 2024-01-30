581be6efed93e69b19dab673282e8b736c47e98d
yewsh <yewsh.lim@gmail.com>
2024-01-30 16:11:37 +0000

    Add better solution and explanation

--- pgexercises/aggregates/10.sql
+++ pgexercises/aggregates/10.sql
@@ -17,3 +17,20 @@ HAVING SUM(
 	  END
 	  ) < 1000
 ORDER BY revenue
+
+
+-- Postgres, unlike some other RDBMSs like SQL Server and MySQL, doesn't support putting column names in the HAVING clause.
+-- Having to repeat significant calculation code like this is messy
+--  In general, recommend using HAVING for simple queries, as it increases clarity. Otherwise, this subquery approach is often easier to use.
+
+select name, revenue from (
+	select facs.name, sum(case 
+				when memid = 0 then slots * facs.guestcost
+				else slots * membercost
+			end) as revenue
+		from cd.bookings bks
+		inner join cd.facilities facs
+			on bks.facid = facs.facid
+		group by facs.name
+	) as agg where revenue < 1000
+order by revenue;     
